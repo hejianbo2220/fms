c465334dd0d23951db6d7acaf95d9cf40a2381ed
何剑波 <112526920@qq.com>
2017-12-11 02:46:25 +0800

    pc端数据预览

--- src/pc/preview.vue
+++ src/pc/preview.vue
@@ -12,7 +12,7 @@
       <el-col :span="24">
         <el-table :data="table" :stripe="true">
           <el-table-column label="产品编码+批次号" prop="productIdAndBatchId"></el-table-column>
-          <el-table-column label="时间" prop="time"></el-table-column>
+          <!-- <el-table-column label="时间" prop="time"></el-table-column> -->
           <el-table-column label="操作">
             <el-button slot-scope="scope" size="mini" icon="el-icon-news" @click="detail(scope.row)">查看</el-button>
           </el-table-column>
@@ -21,41 +21,116 @@
     </el-row>
     <el-row>
       <el-col :span="24">
-        <el-pagination @current-change="pageChanged" :total="tableTotal"></el-pagination>
+        <el-pagination :current-page.sync="currentPage" @current-change="getTable" :total="tableTotal"></el-pagination>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import axios from '@/axios'
 export default{
   name: 'preview',
   data () {
     return {
-      table: [
-        {
-          productIdAndBatchId: 'productId1_BatchId1',
-          time: '2017-11-11 11:11:11'
-        },
-        {
-          productIdAndBatchId: 'productId2_BatchId2',
-          time: '2017-12-12 12:12:12'
-        }
-      ],
-      tableTotal: 89
+      table: [],
+      currentPage: 1,
+      tableTotal: 1
     }
   },
   methods: {
-    detail () {
-      this.$alert('<strong>这里是数据预览详情内容</strong>', {
-        title: '数据预览详情',
-        dangerouslyUseHTMLString: true,
-        showConfirmButton: false
-      }).catch(() => {})
+    getTable (startPage) {
+      axios(this, {
+        msgType: 5,
+        startNo: startPage - 1,
+        num: 10
+      }).then(data => {
+        data.list.forEach(item => {
+          // 产品编码+批次号
+          item.productIdAndBatchId = item.serials + '+' + item.batch
+        })
+        this.table = data.list
+        this.currentPage = startPage
+        this.tableTotal = data.total
+      })
     },
-    pageChanged (page) {
-      console.log(page)
+    detail (data) {
+      axios(this, {
+        msgType: 106,
+        serials: data.serials,
+        batch: data.batch
+      }).then(data => {
+        const h = this.$createElement
+        const list = []
+
+        // 关键数据
+        if (data.key_datas.length > 0) {
+          list.push(h('li', null, '关键数据：'))
+          data.key_datas.forEach(item => {
+            list.push(h('li', {style: 'padding: 10px 0 0 10px'}, '提交人：' + item.user))
+            const dateTemp = new Date()
+            dateTemp.setTime(item.time)
+            list.push(h('li', {style: 'padding-left: 10px'}, '提交时间：' + dateTemp.toLocaleDateString()))
+            item.list.forEach(procedure => {
+              list.push(h('li', {style: 'padding-left: 20px'}, procedure.name + '：'))
+              procedure.list.forEach(attr => {
+                list.push(h('li', {style: 'padding-left: 30px'}, attr.name + '：' + attr.value))
+              })
+            })
+          })
+        }
+
+        // 自检自测
+        if (data.self_ins.length > 0) {
+          list.push(h('li', {style: 'padding-top: 20px'}, '自检自测：'))
+          data.self_ins.forEach(item => {
+            list.push(h('li', {style: 'padding: 10px 0 0 10px'}, '提交人：' + item.user))
+            const dateTemp = new Date()
+            dateTemp.setTime(item.time)
+            list.push(h('li', {style: 'padding-left: 10px'}, '提交时间：' + dateTemp.toLocaleDateString()))
+            item.list.forEach(attr => {
+              list.push(h('li', {style: 'padding-left: 20px'}, attr.name + '：' + attr.value))
+            })
+          })
+        }
+
+        // 质量检测
+        if (data.qa_ins.length > 0) {
+          list.push(h('li', {style: 'padding-top: 20px'}, '质量检测：'))
+          data.qa_ins.forEach(item => {
+            list.push(h('li', {style: 'padding: 10px 0 0 10px'}, '提交人：' + item.user))
+            const dateTemp = new Date()
+            dateTemp.setTime(item.time)
+            list.push(h('li', {style: 'padding-left: 10px'}, '提交时间：' + dateTemp.toLocaleDateString()))
+            item.list.forEach(attr => {
+              list.push(h('li', {style: 'padding-left: 20px'}, attr.name + '：' + attr.value))
+            })
+          })
+        }
+
+        // 问题列表
+        if (data.problem_list.length > 0) {
+          list.push(h('li', {style: 'padding-top: 20px'}, '问题列表：'))
+          data.problem_list.forEach(item => {
+            list.push(h('li', {style: 'padding-left: 10px'}, '问题名称：' + item.title))
+            list.push(h('li', {style: 'padding-left: 20px'}, '问题来源：' + item.source))
+            list.push(h('li', {style: 'padding-left: 20px'}, '责任人：' + item.person))
+            list.push(h('li', {style: 'padding-left: 20px'}, '问题描述：' + item.desc))
+            list.push(h('li', {style: 'padding-left: 20px'}, '解决方案：' + item.programe))
+            list.push(h('li', {style: 'padding-left: 20px'}, '改进措施：' + item.improv))
+          })
+        }
+        this.$msgbox({
+          title: '数据预览详情',
+          message: h('ul', null, list),
+          showConfirmButton: false,
+          closeOnClickModal: false
+        }).catch(() => {})
+      })
     }
+  },
+  mounted () {
+    this.getTable(1)
   }
 }
 </script>
