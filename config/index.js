54eb41e455a8747f9303088f83e56631e62ca3f8
DESKTOP-U6CJKPF\Jason <112526920@qq.com>
2018-01-18 16:16:40 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -55,7 +55,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // assetsPublicPath: '/',
+    assetsPublicPath: '/file/html/',
 
     /**
      * Source Maps
--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,9 @@ export default new Vuex.Store({
   state: {
     token: sessionStorage.getItem('token') ? sessionStorage.getItem('token') : '',
     permission: sessionStorage.getItem('permission') ? sessionStorage.getItem('permission') : '',
-    api: 'http://192.168.10.140:8025/WaterProof/Server',
-    imgUpload: 'http://192.168.10.140:8025/WaterProof/upFileServer',
-    excelUpload: 'http://192.168.10.140:8025/WaterProof/upExcel'
+    api: 'http://192.168.10.140:36000/WaterProof/Server',
+    imgUpload: 'http://192.168.10.140:36000/WaterProof/upFileServer',
+    excelUpload: 'http://192.168.10.140:36000/WaterProof/upExcel'
   },
   mutations: {
     set (state, info) {
